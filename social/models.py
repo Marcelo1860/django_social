4f5b21c6fd83e0cceefdd00dfa63e5a27609af93
Marcelo1860 <chelobader@gmail.com>
2022-12-11 19:14:32 -0300

    Adding images and serving static files

--- social/models.py
+++ social/models.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    #image
+    image = models.ImageField(default='batman.png')
 
     def __str__(self):
         return f'Perfil de {self.user.username}'
--- social/urls.py
+++ social/urls.py
@@ -2,8 +2,12 @@ from django.urls import path
 
 from social.views import profile
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+
 
 urlpatterns = [
     path('', views.feed,name = 'feed'),
     path('profile/',views.profile, name = 'profile'),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
