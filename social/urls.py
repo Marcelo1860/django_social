a492f2e778159d5723dbaa011dbd3e5647e1861b
Marcelo1860 <chelobader@gmail.com>
2022-12-11 23:05:54 -0300

    Adding login and logout

--- social/urls.py
+++ social/urls.py
@@ -4,11 +4,13 @@ from social.views import profile
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.contrib.auth.views import LoginView, LogoutView
 
 
 urlpatterns = [
     path('', views.feed,name = 'feed'),
     path('profile/',views.profile, name = 'profile'),
     path('register/',views.register,name = 'register'),
+    path('login/',LoginView.as_view(template_name='social/login.html'),name='login'),
+    path('logout/',LogoutView.as_view(template_name='social/logout.html'),name='logout'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
