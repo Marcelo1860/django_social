cc647612891f7fea46c05bbc37c6c4d60c5aa558
Marcelo1860 <chelobader@gmail.com>
2022-12-11 23:59:22 -0300

    Adding Profile after User registration using signals

--- social/models.py
+++ social/models.py
@@ -2,7 +2,10 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
 
+
 # Create your models here.
+
+
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='batman.png')
@@ -20,3 +23,5 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.user.username}:{self.content}'
+
+
