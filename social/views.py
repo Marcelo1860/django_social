3c5cabc989deddd27af06c7b77e61d4d45530b13
Marcelo1860 <chelobader@gmail.com>
2022-12-12 16:39:40 -0300

    Adding functionality to follow button and to MyProfile link in navigation bar

--- social/views.py
+++ social/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render, redirect,get_object_or_404
+
+from social.models import Relationship
 from .models import Post 
+from .models import Relationship
 from .forms import UserRegisterForm,PostForm
 from django.contrib import messages
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required 
 # Create your views here.
 def feed(request):
     posts = Post.objects.all()
@@ -22,7 +26,8 @@ def register(request):
         form = UserRegisterForm()
     context = {'form': form}
     return render(request,'social/register.html',context)
-
+    
+@login_required
 def post(request):
     current_user = get_object_or_404 (User, pk=request.user.pk)
     if request.method =='POST':
@@ -37,6 +42,7 @@ def post(request):
         form = PostForm()
     return render(request, 'social/post.html',{'form':form})
 
+
 def profile(request, username=None):
     current_user=request.user
     if username and username != current_user.username:
@@ -46,3 +52,21 @@ def profile(request, username=None):
         posts = current_user.posts.all()
         user = current_user
     return render(request,'social/profile.html',{'user':user,'posts':posts})
+
+def follow(request, username):
+    current_user = request.user
+    to_user = User.objects.get(username = username)
+    to_user_id = to_user
+    rel = Relationship(from_user = current_user, to_user = to_user_id)
+    rel.save()
+    messages.success(request, f'Following {username}')
+    return redirect('feed')
+
+def unfollow(request, username):
+    current_user = request.user
+    to_user = User.objects.get(username = username)
+    to_user_id = to_user.id
+    rel = Relationship.objects.filter(from_user=current_user.id, to_user=to_user_id).get()
+    rel.delete()
+    messages.success(request, f'You dont follow {username} anymore')
+    return redirect('feed')
